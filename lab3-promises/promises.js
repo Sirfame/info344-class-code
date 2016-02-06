aba46b8d554e706f582b0774d20e13f05c965af3
Sirfame Lin <sifame@hotmail.com>
2016-02-06 14:04:31 -0800

    In Class stuff

--- lab3-promises/promises.js
+++ lab3-promises/promises.js
@@ -1,26 +1,43 @@
 'use strict';
 
-function add2(num) {
-  return num;
+//Problem 1
+// function add2(value) {
+//   return new Promise(function(resolve, reject) {
+//     resolve(value);
+//   }).then(function(value) {
+//     return value + 1;
+//   }).then(function(value) {
+//     return value + 1;
+//   }).then(function(value) {
+//     console.log(value);
+//   }).catch(function(err) {
+//     console.error(err);
+//   });
+// }
+//
+// add2(1);
+
+//Problem 2
+var http = require('http');
+
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual request stuff
+    http.get(url, function(res) {
+        var body = '';
+        res.on('data', function(chunk) {
+            body += chunk;
+        });
+        res.on('end', function() {
+            resolve(body);
+        }).then(function(body) {
+          console.log(body);
+        });
+    }).on('error', function(err) {
+        reject(err);
+    });
+  });
 }
 
-var number;
-
-var promise = new Promise(function(resolve, reject) {
-  add2(1)
-  .then(function(num) {
-    number = num + 1;
-    return number;
-  });
-  if (number === 1) {
-    resolve("Stuff worked!");
-  } else {
-    reject(Error("It broke"));
-  }
-});
-
-promise.then(function(result) {
-  console.log(result); // "Stuff worked!"
-}, function(err) {
-  console.log(err); // Error: "It broke"
-});
+get('http://www.omdbapi.com/?i=tt0120737&plot=short&r=json');
---
+++ node-auth/bcrypt.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var bluebird = require('bluebird');
+var bcrypt = bluebird.promisifyAll(require('bcrypt'));
+
+var password = process.argv[2];
+var rounds = 10;
+
+if(process.argv.length >= 4) {
+  rounds = parseInt(process.argv[3]);
+  if(isNaN(rounds)) {
+    console.error('numer of rounds must be an interger!');
+    process.exit(1);
+  }
+}
+
+console.log("hashing '$s' with %d rounds of bcrypt...", password, rounds);
+console.time('duration');
+
+//returns a new promise so that you can call .then
+bcrypt.hashAsync(password, rounds)
+  .then(function(hash) {
+    console.timeEnd('duration');
+    console.log(hash);
+
+    return[hash, bcrypt.compareAsync(password, hash)];
+  })
+  .spread(function(hash, isSame) {
+    console.log("comparing hash against '%s': %j", password, isSame);
+    password += 'x';
+    return [hash, bcrypt.compareAsync(password, hash)];
+  }).spread(function(hash, isSame) {
+    console.log("comparing hash against '%s': %j", password, isSame);
+  }).catch(function(err) {
+    console.error(err);
+    process.exit(1);
+  });
