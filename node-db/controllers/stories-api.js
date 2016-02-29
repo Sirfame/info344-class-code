2862702ece36b9b532e9feae394c18bab2c1f0aa
Sirfame Lin <sifame@hotmail.com>
2016-02-29 10:24:13 -0800

    in class stuff

--- node-db/controllers/stories-api.js
+++ node-db/controllers/stories-api.js
@@ -104,55 +104,164 @@ module.exports.Router = function(stories) {
 
 
 
+
+
+
+
+
+
+
 //What i did in class
 
-// var express = require('express');
-// var request = require('request');
-// var cheerio = require('cheerio');
-// //Returns new express router
-// module.exports.Router = function(Story) {
-//   var router = express.Router();
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+//Returns new express router
+module.exports.Router = function(Story) {
+  var router = express.Router();
+
+  router.get('/stories', function(req, res, next) {
+    //return all stories from the database
+    Story.getAll()
+      .then(function(rows) {
+        res.json(rows);
+      })
+      .catch(next);
+  });
+
+  router.post('/stories', function(req, res, next) {
+    //insert a new story into the database
+    //and return the data with default values
+    //applied
+    request.get(req.body.url, function(err, response, body) {
+      if(err) {
+        req.body.title = req.body.url;
+      } else {
+        var $ = cheerio.load(body);
+        req.body.title = $('head title').text();
+      }
+      Story.insert(req.body)
+        .then(function(rows) {
+          res.json(rows);
+        })
+        .catch(next);
+    });
+
+
+  });
+
+  //after colon (":id") needs to match req.params____
+  router.post('/stories/:id/votes', function(req, res, next) {
+    //upvote the story and return the
+    //full story with current number of votes
+    Story.upVote(req.params.id)
+      .then(function(row) {
+        res.json(row);
+      })
+      .catch(next);
+  });
+
+  return router;
+}
+
+
 //
-//   router.get('/stories', function(req, res, next) {
-//     //return all stories from the database
-//     Story.getAll()
-//       .then(function(rows) {
-//         res.json(rows);
-//       })
-//       .catch(next);
-//   });
+// module.exports.Router = function(passport) {
 //
-//   router.post('/stories', function(req, res, next) {
-//     //insert a new story into the database
-//     //and return the data with default values
-//     //applied
-//     request.get(req.body.url, function(err, response, body) {
-//       if(err) {
-//         req.body.title = req.body.url;
-//       } else {
-//         var $ = cheerio.load(body);
-//         req.body.title = $('head title').text();
-//       }
-//       Story.insert(req.body)
-//         .then(function(rows) {
-//           res.json(rows);
-//         })
-//         .catch(next);
+// 	// =========================================================================
+//     // passport session setup ==================================================
+//     // =========================================================================
+//     // required for persistent login sessions
+//     // passport needs ability to serialize and unserialize users out of session
+//
+//     // used to serialize the user for the session
+//     passport.serializeUser(function(user, done) {
+// 		done(null, user.id);
+//     });
+//
+//     // used to deserialize the user
+//     passport.deserializeUser(function(id, done) {
+// 		connection.query("select * from users where id = "+id,function(err,rows){
+// 			done(err, rows[0]);
+// 		});
 //     });
 //
 //
-//   });
+//  	// =========================================================================
+//     // LOCAL SIGNUP ============================================================
+//     // =========================================================================
+//     // we are using named strategies since we have one for login and one for signup
+// 	// by default, if there was no name, it would just be called 'local'
 //
-//   //after colon (":id") needs to match req.params____
-//   router.post('/stories/:id/votes', function(req, res, next) {
-//     //upvote the story and return the
-//     //full story with current number of votes
-//     Story.upVote(req.params.id)
-//       .then(function(row) {
-//         res.json(row);
-//       })
-//       .catch(next);
-//   });
+//     passport.use('local-signup', new LocalStrategy({
+//         // by default, local strategy uses username and password, we will override with email
+//         usernameField : 'email',
+//         passwordField : 'password',
+//         passReqToCallback : true // allows us to pass back the entire request to the callback
+//     },
+//     function(req, email, password, done) {
 //
-//   return router;
-// }
+// 		// find a user whose email is the same as the forms email
+// 		// we are checking to see if the user trying to login already exists
+//         connection.query("select * from users where email = '"+email+"'",function(err,rows){
+// 			console.log(rows);
+// 			console.log("above row object");
+// 			if (err)
+//                 return done(err);
+// 			 if (rows.length) {
+//                 return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+//             } else {
+//
+// 				// if there is no user with that email
+//                 // create the user
+//                 var newUserMysql = new Object();
+//
+// 				newUserMysql.email    = email;
+//                 newUserMysql.password = password; // use the generateHash function in our user model
+//
+// 				var insertQuery = "INSERT INTO users ( email, password ) values ('" + email +"','"+ password +"')";
+// 					console.log(insertQuery);
+// 				connection.query(insertQuery,function(err,rows){
+// 				newUserMysql.id = rows.insertId;
+//
+// 				return done(null, newUserMysql);
+// 				});
+//             }
+// 		});
+//     }));
+//
+//     // =========================================================================
+//     // LOCAL LOGIN =============================================================
+//     // =========================================================================
+//     // we are using named strategies since we have one for login and one for signup
+//     // by default, if there was no name, it would just be called 'local'
+//
+//     passport.use('local-login', new LocalStrategy({
+//         // by default, local strategy uses username and password, we will override with email
+//         usernameField : 'email',
+//         passwordField : 'password',
+//         passReqToCallback : true // allows us to pass back the entire request to the callback
+//     },
+//     function(req, email, password, done) { // callback with email and password from our form
+//
+//          connection.query("SELECT * FROM `users` WHERE `email` = '" + email + "'",function(err,rows){
+// 			if (err)
+//                 return done(err);
+// 			 if (!rows.length) {
+//                 return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+//             }
+//
+// 			// if the user is found but the password is wrong
+//             if (!( rows[0].password == password))
+//                 return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+//
+//             // all is well, return successful user
+//             return done(null, rows[0]);
+//
+// 		});
+//
+//
+//
+//     }));
+//
+// };
